0abd1b0c7da1f8567c2f6466dc02302fe7838a9a
amadodiazjr <amado.diaz.jr@gmail.com>
2014-10-11 16:42:26 -0400

    implemented QuickSort

--- src/main/java/com/amado/QuickSorter.java
+++ src/main/java/com/amado/QuickSorter.java
@@ -1,5 +1,42 @@
 package com.amado;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class QuickSorter {
 
+	public List<Integer> sort(final List<Integer> unsortedIntegers) {
+		final List<Integer> sortedIntegers = new ArrayList<Integer>();
+		if (null == unsortedIntegers || unsortedIntegers.isEmpty()) {
+			return sortedIntegers;
+		}
+		
+		if (unsortedIntegers.size()==1) {
+			return unsortedIntegers;
+		}			
+		
+		final Integer pivotIndex = unsortedIntegers.size() / 2;
+		final Integer pivotValue = unsortedIntegers.get(pivotIndex);
+		
+		final List<Integer> smallerIntegers = new ArrayList<Integer>();
+		final List<Integer> biggerIntegers = new ArrayList<Integer>();
+		for (int i=0; i<unsortedIntegers.size(); i++) {
+			if (pivotIndex.equals(i)) {
+				continue;
+			}
+			
+			final Integer unsortedInteger = unsortedIntegers.get(i);
+			if (unsortedInteger.compareTo(pivotValue) < 0) {
+				smallerIntegers.add(unsortedInteger);
+			} else {
+				biggerIntegers.add(unsortedInteger);
+			}
+		}
+		
+		sortedIntegers.addAll(sort(smallerIntegers));
+		sortedIntegers.add(pivotValue);
+		sortedIntegers.addAll(sort(biggerIntegers));
+		
+		return sortedIntegers;
+	}
 }
--- src/test/java/com/amado/unit/QuickSorterTest.java
+++ src/test/java/com/amado/unit/QuickSorterTest.java
@@ -4,6 +4,9 @@ import static org.hamcrest.Matchers.is;
 import static org.hamcrest.Matchers.notNullValue;
 import static org.junit.Assert.assertThat;
 
+import java.util.Arrays;
+import java.util.List;
+
 import org.junit.Test;
 
 import com.amado.QuickSorter;
@@ -20,6 +23,27 @@ public class QuickSorterTest {
 		
 		// ~then
 		assertThat(sorter, is(notNullValue()));
+	}
+
+	@Test
+	public void canSort() {
+		// ~given
+		final Integer[] unsortedIntegers = {3, 7, 8, 5, 2, 1, 9, 5, 4};
+		final QuickSorter sorter = new QuickSorter();
+
+		// ~when
+		final List<Integer> sortedIntegers = sorter.sort(Arrays.asList(unsortedIntegers));
+		
+		// ~then {1, 2, 3, 4, 5, 5, 7, 8, 9};
+		assertThat(sortedIntegers.get(0), is(1));
+		assertThat(sortedIntegers.get(1), is(2));
+		assertThat(sortedIntegers.get(2), is(3));
+		assertThat(sortedIntegers.get(3), is(4));
+		assertThat(sortedIntegers.get(4), is(5));
+		assertThat(sortedIntegers.get(5), is(5));
+		assertThat(sortedIntegers.get(6), is(7));
+		assertThat(sortedIntegers.get(7), is(8));
+		assertThat(sortedIntegers.get(8), is(9));
 	}
 
 }
