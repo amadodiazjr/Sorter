398a71d681cd48b131f624276320f380d56f247d
amadodiazjr <amado.diaz.jr@gmail.com>
2014-10-12 17:15:38 -0400

    finished merge sort - better way to write this

--- src/main/java/com/amado/MergeSorter.java
+++ src/main/java/com/amado/MergeSorter.java
@@ -1,5 +1,96 @@
 package com.amado;
 
-public class MergeSorter {
+import java.util.ArrayList;
+import java.util.List;
 
-}
+public class MergeSorter {
+	private final List<List<Integer>> _sortedLists;
+	
+	public MergeSorter() {
+		_sortedLists = new ArrayList<List<Integer>>();
+	}
+	
+	public List<Integer> sort(final List<Integer> unsortedIntegers) {
+		if (null == unsortedIntegers) {
+			return new ArrayList<Integer>();
+		}
+		
+		split(unsortedIntegers);
+		
+		return merge(_sortedLists);
+	}
+	
+	private void split(final List<Integer> integers) {
+		if (null == integers) {
+			return;
+		}
+		
+		if (1 == integers.size()) {
+			_sortedLists.add(integers);
+			return;
+		}
+		
+		final List<Integer> aList = new ArrayList<Integer>();
+		final List<Integer> bList = new ArrayList<Integer>();
+		final Integer midIndex = integers.size() / 2;
+		for (int i=0; i<midIndex; i++) {
+			aList.add(integers.get(i));
+		}
+
+		for (int i=midIndex; i<integers.size(); i++) {
+			bList.add(integers.get(i));
+		}
+		
+		split(aList);
+		split(bList);
+	}
+	
+	private List<Integer> merge(final List<List<Integer>> sortedLists) {
+		if (1 == sortedLists.size()) {
+			return sortedLists.get(0);
+		}
+
+		final List<List<Integer>> newSortedLists = new ArrayList<List<Integer>>();
+		final Integer size = sortedLists.size(); 
+				
+		Integer i = 0;
+		while (i < size) {
+			Integer aIndex = 0;
+			Integer bIndex = 0;
+			
+			final List<Integer> aList = sortedLists.get(i);
+			i++;
+			final List<Integer> bList = (i < size) ? sortedLists.get(i) : new ArrayList<Integer>();
+			final List<Integer> mergedList = new ArrayList<Integer>();
+
+			while (aIndex < aList.size() || bIndex < bList.size()) {
+				final Integer aValue = (aIndex < aList.size()) ? aList.get(aIndex) : null;
+				final Integer bValue = (bIndex < bList.size()) ? bList.get(bIndex) : null;
+				
+				if (null != aValue && null != bValue) {
+					if (aValue.compareTo(bValue) < 1) {
+						mergedList.add(aValue);
+						aIndex++;
+					} else {
+						mergedList.add(bValue);
+						bIndex++;
+					}
+				} else if (null == aValue && null != bValue) {
+					mergedList.add(bValue);
+					bIndex++;
+				} else if (null != aValue && null == bValue) {
+					mergedList.add(aValue);
+					aIndex++;
+				} else if (null == aValue && null == bValue) {
+					// do nothing
+				}				
+			}
+				
+			newSortedLists.add(mergedList);
+			i++;
+		}
+		
+		return merge(newSortedLists);
+	}
+
+ }
--- src/test/java/com/amado/unit/MergeSorterTest.java
+++ src/test/java/com/amado/unit/MergeSorterTest.java
@@ -4,6 +4,9 @@ import static org.hamcrest.Matchers.is;
 import static org.hamcrest.Matchers.notNullValue;
 import static org.junit.Assert.assertThat;
 
+import java.util.Arrays;
+import java.util.List;
+
 import org.junit.Test;
 
 import com.amado.MergeSorter;
@@ -24,7 +27,21 @@ public class MergeSorterTest {
 
 	@Test
 	public void canSort() {
+		// ~given
+		final Integer[] unsortedIntegers = {6, 5, 3, 1, 7, 2, 4};
+		final MergeSorter sorter = new MergeSorter();
 
+		// ~when
+		final List<Integer> sortedIntegers = sorter.sort(Arrays.asList(unsortedIntegers));
+		
+		// ~then {1, 2, 3, 4, 5, 5, 7};
+		assertThat(sortedIntegers.get(0), is(1));
+		assertThat(sortedIntegers.get(1), is(2));
+		assertThat(sortedIntegers.get(2), is(3));
+		assertThat(sortedIntegers.get(3), is(4));
+		assertThat(sortedIntegers.get(4), is(5));
+		assertThat(sortedIntegers.get(5), is(6));
+		assertThat(sortedIntegers.get(6), is(7));
 	}
 
 }
